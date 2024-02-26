037c6a2abfeef82597bd53debc94539daf5e87f1
Shivam <mishra.shiva999@gmail.com>
2024-02-26 11:56:39 +0530

    display full name

---
+++ display-name/src/App.js
@@ -0,0 +1,12 @@
+import Display from "./components/Display";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Display />
+    </div>
+  );
+}
+
+export default App;
---
+++ display-name/src/components/Display.jsx
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import styles from './Display.module.css';
+
+export default function Display() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [fullName, setFullName] = useState("");
+
+    const handleFirstName = (event) => {
+        setFirstName(event.target.value);
+    }
+
+    const handleLastName = (event) => {
+        setLastName(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if(!firstName || !lastName){
+            alert("please fill out this field");
+            return;
+        }
+        const fullname = `${firstName} ${lastName}`;
+        setFullName(fullname);  
+    }
+
+  return (
+    <div>
+      <form className={styles.form}>
+        <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input id="firstName" type="text" value={firstName} onChange={handleFirstName}></input>
+        </div>
+
+        <div>
+            <label htmlFor="lastName">Last Name:</label>
+            <input id="lastName" type="text" value={lastName} onChange={handleLastName}></input>
+        </div>
+
+        <div>
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+      </form>
+
+      <div className={styles.output}>
+        {fullName && 
+            <p>Full Name: {fullName}</p>
+        }
+      </div>
+    </div>
+  )
+}
